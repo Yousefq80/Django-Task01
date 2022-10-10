03a1fad3907f6fb2dfbbef066ba8d262f2127b6f
Yousefq80 <ysalharby@gmail.com>
2022-10-11 00:26:33 +0300

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,7 @@
+from email.policy import default
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    body = models.TextField(default="")
